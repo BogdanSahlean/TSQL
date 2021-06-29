b6deb2dd39729921a63378112968923c67d88a26
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-06-29 11:21:49 +0300

    Update sp_who4.sql

--- sp_who4.sql
+++ sp_who4.sql
@@ -10,7 +10,7 @@ CREATE PROC sp_who4
 @get_indexes INT = NULL --NULL=No, 1=XML, 2=SQL Statements
 AS
 BEGIN
-	SET NOCOUNT ON            
+	SET NOCOUNT ON               
    
 	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED 
 
