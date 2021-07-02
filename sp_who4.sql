2ca3897a3b108e1b15de59c90746d87ba111373c
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-07-02 11:41:00 +0300

    Update sp_who4.sql

--- sp_who4.sql
+++ sp_who4.sql
@@ -47,7 +47,7 @@ BEGIN
 	AS (
 		SELECT	blk_ses.group_num, CONVERT(HIERARCHYID, '/' + LTRIM(blk_ses.session_id) + '/') AS hid, blk_ses.session_id, blk_ses.blocked_by
 		FROM	BlkSessions blk_ses   
-		WHERE	blk_ses.blocked_by IS NULL                                                                                                                                                                                 
+		WHERE	blk_ses.blocked_by IS NULL                                                                                                                                                                                    
 		UNION ALL
 		SELECT	blk_hd.group_num, CONVERT(HIERARCHYID, blk_hd.hid.ToString() + LTRIM(blk_ses.session_id) + '/') AS hid, blk_ses.session_id, blk_ses.blocked_by
 		FROM	BlkSessionsRecursion blk_hd 
