02c14fd140e6a38ba1700e915f4c2d1314861a68
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-07-17 22:50:54 +0300

    Update sp_who4.sql

--- sp_who4.sql
+++ sp_who4.sql
@@ -52,7 +52,7 @@ BEGIN
 		
 		
 		WHERE	blk_ses.blocked_by IS NULL                                                                                                                                                                                                                                                                                   
-		UNION ALL      
+		UNION ALL         
 		SELECT	blk_hd.group_num, CONVERT(HIERARCHYID, blk_hd.hid.ToString() + LTRIM(blk_ses.session_id) + '/') AS hid, blk_ses.session_id, blk_ses.blocked_by
 		FROM	BlkSessionsRecursion blk_hd 
 		JOIN	BlkSessions blk_ses ON blk_ses.blocked_by = blk_hd.session_id            
