620de769161175f9d4223d7465742815c9fbc035
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-07-17 22:55:46 +0300

    Update xdl.sql

--- xdl.sql
+++ xdl.sql
@@ -52,9 +52,9 @@ RETURN
 SELECT	*   
 FROM	(
 	SELECT	'ipbuffer' name, spid.Nod.value('(@spid)[1]', 'int') spid, excst.Nod.value('.', 'NVARCHAR(MAX)') ipbuffer
-	FROM	@dl.nodes('deadlock-list/deadlock/process-list/process') spid(Nod)                                                                           
+	FROM	@dl.nodes('deadlock-list/deadlock/process-list/process') spid(Nod)                                                                              
 	CROSS APPLY spid.Nod.nodes('executionStack/frame') excst(Nod)   
-) cox        
+) cox           
 PIVOT( MAX(ipbuffer) FOR spid IN ([182], [175]) ) pvot   
 
 IF OBJECT_ID('tempdb..#cox') IS NOT NULL
