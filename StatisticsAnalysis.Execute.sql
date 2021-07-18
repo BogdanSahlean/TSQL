e2d8749fd40ac5e1cc8dfd62496191375aaef336
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-07-18 13:52:42 +0300

    Update StatisticsAnalysis.Execute.sql

--- StatisticsAnalysis.Execute.sql
+++ StatisticsAnalysis.Execute.sql
@@ -51,7 +51,7 @@ DECLARE @plan XML = N'<?xml version="1.0" encoding="utf-16"?>
                 
                 
               
-                  <DefinedValue>      
+                  <DefinedValue>         
                     <ColumnReference Column="Expr1002" />                  
                     <ScalarOperator ScalarString="CONVERT_IMPLICIT(int,[globalagg1005],0)">
                       <Convert DataType="int" Style="0" Implicit="true">   
