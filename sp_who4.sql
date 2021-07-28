bc38c9a79d2fe75c7059d8f20c461eaf816e931e
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-07-28 13:41:54 +0300

    Update sp_who4.sql

--- sp_who4.sql
+++ sp_who4.sql
@@ -167,7 +167,7 @@ BEGIN
 	DECLARE @SqlStatement NVARCHAR(MAX) = ''   
 	SELECT	@SqlStatement = @SqlStatement  + 'UNION SELECT hid = ' + CONVERT(VARCHAR(200), CONVERT(VARBINARY(8000), CONVERT(HIERARCHYID, cto.hid)), 1) + ', obct_name = (SELECT QUOTENAME(DB_NAME(' + LTRIM(cto.dbid) + ')) + ''.'' + QUOTENAME(OBJECT_SCHEMA_NAME(pos.object_id)) + ''.'' + QUOTENAME(OBJECT_NAME(pos.object_id)) FROM ' + QUOTENAME(DB_NAME(cto.dbid)) + '.sys.partitions pos WHERE pos.partition_id = ' + LTRIM(cto.associatedObjectId) + ')'
 	FROM	#obct_locks cto
-	
+	   
 	SELECT @SqlStatement = SUBSTRING(@SqlStatement, 6, 8000000)
 
 	--Search name of obcts 
