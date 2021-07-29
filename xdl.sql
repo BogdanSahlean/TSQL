2587617570c31163afcc5f0394b6d3ba964e7ff4
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-07-29 08:54:09 +0300

    Update xdl.sql

--- xdl.sql
+++ xdl.sql
@@ -46,8 +46,8 @@ DECLARE @Cols NVARCHAR(MAX) = ''
 SELECT @Cols = STUFF((                         
 SELECT ', ' + QUOTENAME(LTRIM(spid) + '.' + LTRIM(ISNULL(ecid,0)) + '.' + LTRIM(id))
 FROM #cox cox
-WHERE NULLIF(cox.name, '') IS NOT NULL
-GROUP BY spid, ecid, id                                                                   
+WHERE NULLIF(cox.name, '') IS NOT NULL   
+GROUP BY spid, ecid, id                                                                      
 ORDER BY 1
 FOR XML PATH(N''), TYPE
 ).value('.', 'NVARCHAR(MAX)'), 1, 2, '')
