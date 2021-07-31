ff50795004662e807060491175fba2db3a885fcf
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-07-31 07:39:10 +0300

    Update ActualPlan.Execute.sql

--- ActualPlan.Execute.sql
+++ ActualPlan.Execute.sql
@@ -28,7 +28,7 @@ DECLARE @plan XML = N'<?xml version="1.0" encoding="utf-16"?>
               <Wait WaitType="CXPACKET" WaitTimeMs="3" WaitCount="9" />      
               <Wait WaitType="EXECSYNC" WaitTimeMs="3" WaitCount="9" />                           
               <Wait WaitType="RESERVED_MEMORY_ALLOCATION_EXT" WaitTimeMs="2" WaitCount="182" />                                                                                                                                 
-            </WaitStats>      
+            </WaitStats>         
             <QueryTimeStats CpuTime="11398" ElapsedTime="29138" />   
                <RelOp AvgRowSize="11" EstimateCPU="0.0285019" EstimateIO="0" EstimateRebinds="0" EstimateRewinds="0" EstimatedExecutionMode="Row" EstimateRows="1" LogicalOp="Gather Streams" NodeId="0" Parallel="true" PhysicalOp="Parallelism" EstimatedTotalSubtreeCost="31.4377">                                                                                                                                                                     
               <OutputList>   
