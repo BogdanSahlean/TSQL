7b9d67e2b580774ac6c0bc21b4e491017e6ef423
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-08-03 15:43:02 +0300

    Update ActualPlan.Execute.sql

--- ActualPlan.Execute.sql
+++ ActualPlan.Execute.sql
@@ -58,7 +58,7 @@ DECLARE @plan XML = N'<?xml version="1.0" encoding="utf-16"?>
                         <ColumnReference Column="Expr1002" />         
                         <ScalarOperator ScalarString="CONVERT_IMPLICIT(int,[Expr1005],0)">
                           <Convert DataType="int" Style="0" Implicit="true">
-                            <ScalarOperator>   
+                            <ScalarOperator>      
                               <Identifier>   
                                 <ColumnReference Column="Expr1005" />   
                               </Identifier>   
