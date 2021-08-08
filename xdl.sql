128cf7fd80b9cc351b8b15d7fbfa159abade1fca
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-08-08 13:44:47 +0300

    Update xdl.sql

--- xdl.sql
+++ xdl.sql
@@ -54,7 +54,7 @@ FOR XML PATH(N''), TYPE
    
 SELECT @SqlStatement = N'                                                                     
 SELECT *   
-FROM (                                          
+FROM (                                             
 SELECT (LTRIM(spid) + ''.'' + LTRIM(ISNULL(ecid,0)) + ''.'' + LTRIM(id)) id,  [name], [value]
 FROM #cox cox
 UNION      
