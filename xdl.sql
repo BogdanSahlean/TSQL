a72ae5f2527b574b821a631140e76a47ef1b1579
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-08-10 19:39:34 +0300

    Update xdl.sql

--- xdl.sql
+++ xdl.sql
@@ -52,7 +52,7 @@ FOR XML PATH(N''), TYPE
 ).value('.', 'NVARCHAR(MAX)'), 1, 2, '')
       
 SELECT @SqlStatement = N'                                                                     
-SELECT *         
+SELECT *            
 FROM (                                             
 SELECT (LTRIM(spid) + ''.'' + LTRIM(ISNULL(ecid,0)) + ''.'' + LTRIM(id)) id,  [name], [value]
 FROM #cox cox
@@ -107,7 +107,7 @@ SELECT * FROM #rez ORDER BY [name]'
 EXEC sp_executesql @SqlStatement, N'@dl XML', @dl
 
 IF OBJECT_ID('tempdb..#resc') IS NOT NULL                                                                   
-BEGIN
+BEGIN   
 DROP TABLE #resc
 END   
 
