2ade47c519a0aa55cd62eb90d37de20a5183673e
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-08-14 17:50:47 +0300

    Update ActualPlan.Execute.sql

--- ActualPlan.Execute.sql
+++ ActualPlan.Execute.sql
@@ -52,7 +52,7 @@ DECLARE @plan XML = N'<?xml version="1.0" encoding="utf-16"?>
                        <RunTimeCountersPerThread Thread="0" ActualRows="0" Batches="0" ActualEndOfScans="0" ActualExecutions="0" ActualExecutionMode="Row" ActualElapsedms="0" ActualCPUms="0" />
                   </RunTimeInformation>                                                                                   
                   <ComputeScalar                                
-                    <DefinedValues>                                  
+                    <DefinedValues>                                     
                       <DefinedValue>            
                         <ColumnReference Column="Expr1002" />            
                         <ScalarOperator ScalarString="CONVERT_IMPLICIT(int,[Expr1005],0)">      
