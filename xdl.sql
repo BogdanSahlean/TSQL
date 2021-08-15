5f019dfb4e55d37b276c31f5a9a8ba128efbd5c9
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-08-15 13:43:51 +0300

    Update xdl.sql

--- xdl.sql
+++ xdl.sql
@@ -40,7 +40,7 @@ SELECT spid.Nod.value('(@spid)[1]', 'int') spid, spid.Nod.value('(@ecid)[1]', 'i
 FROM @dl.nodes('deadlock-list/deadlock/process-list/process') spid(Nod)
 ) s CROSS APPLY s.cox.nodes('i') i(Nod)
                                                                                                                                                                                                               
-SELECT @SqlStatement = ''                                                                                                                          
+SELECT @SqlStatement = ''                                                                                                                             
 DECLARE @Cols NVARCHAR(MAX) = ''
 SELECT @Cols = STUFF((                                                                       
 SELECT ', ' + QUOTENAME(LTRIM(spid) + '.' + LTRIM(ISNULL(ecid,0)) + '.' + LTRIM(id))     
