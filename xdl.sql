4b50b855a496c3287bef6c0c5298b0e4cf2c8edb
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-08-17 17:57:45 +0300

    Update xdl.sql

--- xdl.sql
+++ xdl.sql
@@ -107,7 +107,7 @@ SELECT * FROM #rez ORDER BY [name]'
 EXEC sp_executesql @SqlStatement, N'@dl XML', @dl
 
 IF OBJECT_ID('tempdb..#resc') IS NOT NULL                                                                   
-                              
+                                    
 BEGIN            
 DROP TABLE #resc
 END   
