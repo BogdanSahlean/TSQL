9f45475af5561420eb83448abefece90c3760f52
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-08-17 07:59:57 +0300

    Update sp_who4.sql

--- sp_who4.sql
+++ sp_who4.sql
@@ -26,7 +26,7 @@ BEGIN
 		SELECT	blk_sei.spid AS session_id, NULLIF(blk_sei.blocked, 0) AS blocked_by, NULL AS group_num      
 		FROM	sys.sysprocesses blk_sei                             
 		WHERE	blk_sei.blocked <> 0                               
-		UNION ALL                             
+		UNION ALL                                
 		SELECT	blk_blk.session_id, NULL AS blocked_by, ROW_NUMBER() OVER(ORDER BY blk_blk.session_id)  AS group_num                                                                                                  
 		FROM (               
 			SELECT	blk_sei.spid AS    session_id   
