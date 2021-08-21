4053128ff9bfc9a503f526e2e13b3b76df25fce0
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-08-21 11:53:53 +0300

    Update xdl.sql

--- xdl.sql
+++ xdl.sql
@@ -59,7 +59,7 @@ FROM #cox cox
 UNION      
 SELECT s.idc, ''deadlockvictim'' [name], ''1'' [value]   
 FROM (      
-SELECT vict.Nod.value(''(@victim)[1]'', ''sysname'')
+SELECT vict.Nod.value(''(@victim)[1]'', ''sysname'')   
 FROM @dl.nodes(''deadlock-list/deadlock'') vict(Nod)      
 ) vict(id)   
 JOIN (
