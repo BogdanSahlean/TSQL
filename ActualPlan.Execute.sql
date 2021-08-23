5580615c7fecd0167033534247f870fee8e874d3
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-08-23 11:49:07 +0300

    Update ActualPlan.Execute.sql

--- ActualPlan.Execute.sql
+++ ActualPlan.Execute.sql
@@ -85,7 +85,7 @@ DECLARE @plan XML = N'<?xml version="1.0" encoding="utf-16"?>
                       <Hash>
                         <DefinedValues>   
                           <DefinedValue>   
-                            <ColumnReference Column="Expr1005" />
+                            <ColumnReference Column="Expr1005" />   
                             <ScalarOperator ScalarString="COUNT(*)">
                               <Aggregate AggType="COUNT*" Distinct="false" />
                             </ScalarOperator>
