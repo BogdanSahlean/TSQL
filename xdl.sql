a421217ff0a198ba8bcf7acd1767af15e43a4b8d
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-08-27 12:43:55 +0300

    Update xdl.sql

--- xdl.sql
+++ xdl.sql
@@ -20,7 +20,7 @@ END
 ELSE IF @SrceType = 2 /*SQL Profiler Table*/ AND @SrceID IS NOT NULL             
 BEGIN     
 	SELECT @SqlStatement = 
-'SELECT	@SrceXml = CONVERT(XML, qprofiler.TextData)    
+'SELECT	@SrceXml = CONVERT(XML, qprofiler.TextData)      
    WHERE	qprofiler.EventClass = (SELECT etns.trace_event_id FROM sys.trace_events etns WHERE	etns.name = ''Deadlock graph'')      
 AND qprofiler.RowNumber = @SrceID'            
 	EXECUTE sp_executesql @SqlStatement, N'@SrceID INT, @SrceXml XML OUTPUT', @SrceID = @SrceID, @SrceXml = @SrceXml OUTPUT           
