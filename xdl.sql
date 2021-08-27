b7c00c82c528b8a02bda37f0ca116b75146d52bf
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-08-27 20:42:54 +0300

    Update xdl.sql

--- xdl.sql
+++ xdl.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 GO
 ALTER PROCEDURE [dbo].[XdlAnalysis]     
-@SrceType	INT, --1 Xdl, 2=SQL Profiler Table
+@SrceType	INT, --1 Xdl, 2=SQL Profiler Table   
       
 @SrceDesc	VARCHAR(MAX),        
 @SrceID		INT   
