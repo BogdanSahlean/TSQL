d75ede4d20884b9956e4748ca57e229b7764a5e3
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-08-29 09:54:20 +0300

    Update StatisticsAnalysis.Execute.sql

--- StatisticsAnalysis.Execute.sql
+++ StatisticsAnalysis.Execute.sql
@@ -58,7 +58,7 @@ DECLARE @plan XML = N'<?xml version="1.0" encoding="utf-16"?>
                             <ColumnReference Column="globalagg1005" />      
                           </Identifier>         
                         </ScalarOperator>        
-                      </Convert>
+                      </Convert>  
                     </ScalarOperator>         
                   </DefinedValue>
                 </DefinedValues>
