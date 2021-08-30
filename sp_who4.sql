178f1b3dbe344ebdee91622dc1d586fb6c650254
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-08-30 10:49:30 +0300

    Update sp_who4.sql

--- sp_who4.sql
+++ sp_who4.sql
@@ -5,10 +5,10 @@ GO
 IF OBJECT_ID('sp_who4') IS NOT NULL      
 BEGIN   
 	DROP PROC sp_who4            
-END        
+END           
 GO           
 CREATE PROC sp_who4           
-@get_indexes INT = NULL --NULL=No, 1=XML, 2=SQL Statements                 
+@get_indexes INT = NULL --NULL=No, 1=XML, 2=SQL Statements                    
 AS               
 BEGIN         
 	SET NOCOUNT ON                            
