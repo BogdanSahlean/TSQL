df02e26a9d2ba3fa3e6d27ca5893ab55bd25fe69
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-08-31 15:55:47 +0300

    Update xdl.sql

--- xdl.sql
+++ xdl.sql
@@ -23,7 +23,7 @@ BEGIN
 'SELECT	@SrceXml = CONVERT(XML, qprofiler.TextData)         
    WHERE	qprofiler.EventClass = (SELECT etns.trace_event_id FROM sys.trace_events etns WHERE	etns.name = ''Deadlock graph'')      
 AND qprofiler.RowNumber = @SrceID'              
-	EXECUTE sp_executesql @SqlStatement, N'@SrceID INT, @SrceXml XML OUTPUT', @SrceID = @SrceID, @SrceXml = @SrceXml OUTPUT           
+	EXECUTE sp_executesql @SqlStatement, N'@SrceID INT, @SrceXml XML OUTPUT', @SrceID = @SrceID, @SrceXml = @SrceXml OUTPUT              
 END                 
                  
 DECLARE @xdl NVARCHAR(MAX) = CASE WHEN @SrceXml IS NOT NULL THEN CONVERT(VARCHAR(MAX), @SrceXml) ELSE @SrceDesc END                                    
