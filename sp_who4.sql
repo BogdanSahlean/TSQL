443288a1c8692782200ea285ba97de96b110997b
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-08-31 12:50:02 +0300

    Update sp_who4.sql

--- sp_who4.sql
+++ sp_who4.sql
@@ -10,7 +10,7 @@ GO
 CREATE PROC sp_who4   
 @get_indexes INT = NULL --NULL=No, 1=XML, 2=SQL Statements                          
 AS               
-BEGIN                     
+BEGIN                  
 	SET NOCOUNT ON                                  
         
 	SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED                         
@@ -27,9 +27,9 @@ BEGIN
 		FROM	sys.sysprocesses blk_sei                                
 		WHERE	blk_sei.blocked <> 0                                     
 		UNION ALL                                  
-		SELECT	blk_blk.session_id, NULL AS blocked_by, ROW_NUMBER() OVER(ORDER BY blk_blk.session_id)  AS group_num                                                                                                           
+		SELECT	blk_blk.session_id, NULL AS blocked_by, ROW_NUMBER() OVER(ORDER BY blk_blk.session_id)  AS group_num                                                                                                              
 		FROM (                        
-			SELECT	blk_sei.spid AS    session_id                                   
+			SELECT	blk_sei.spid AS    session_id                                      
 			FROM	sys.sysprocesses blk_sei                                                                                                                                        
 			WHERE	EXISTS(SELECT * FROM s   ys.dm_os_waiting_tasks dmowt WHERE dmowt.blocking_session_id = blk_sei.spid) -- blk_sei.blocked = 0                                                                                                                                                                                                                    
 			AND		NOT EXISTS(SELECT * FROM sys.dm_os_waiting_tasks dmowt WHERE dmowt.session_id = blk_sei.spid) -- blk_sei.blocked = 0
