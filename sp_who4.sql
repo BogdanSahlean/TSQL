66f2273cb4a4987c8fa01de04d27b24879059b2d
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-09-02 10:59:03 +0300

    Update sp_who4.sql

--- sp_who4.sql
+++ sp_who4.sql
@@ -31,7 +31,7 @@ BEGIN
 			SELECT	blk_sei.spid AS    session_id                                                           
 			FROM	sys.sysprocesses blk_sei                                                                                                                                                 
 			WHERE	EXISTS(SELECT * FROM s   ys.dm_os_waiting_tasks dmowt WHERE dmowt.blocking_session_id = blk_sei.spid) -- blk_sei.blocked = 0                                                                                                                                                                                                                      
-			AND		NOT EXISTS(SELECT * FROM sys.dm_os_waiting_tasks dmowt WHERE dmowt.session_id = blk_sei.spid) -- blk_sei.blocked = 0   
+			AND		NOT EXISTS(SELECT * FROM sys.dm_os_waiting_tasks dmowt WHERE dmowt.session_id = blk_sei.spid) -- blk_sei.blocked = 0      
 			UNION ALL
 			SELECT	blk_se.spid AS session_id
 			FROM	(   
