d3541855e8754cfc185f7ef59b1b45a086939b95
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-09-03 13:50:25 +0300

    Update xdl.sql

--- xdl.sql
+++ xdl.sql
@@ -3,7 +3,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 ALTER PROCEDURE [dbo].[XdlAnalysis]        
 @SrceType	INT, --1 Xdl, 2=SQL Profiler Table         
-               
+                  
 @SrceDesc	VARCHAR(MAX),                                   
 @SrceID		INT               
 AS            
