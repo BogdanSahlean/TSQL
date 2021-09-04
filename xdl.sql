bc1921e8d1556a522897bab62ed8ecb5369ac533
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-09-04 13:48:45 +0300

    Update xdl.sql

--- xdl.sql
+++ xdl.sql
@@ -27,7 +27,7 @@ AND qprofiler.RowNumber = @SrceID'
 END                    
                        
 DECLARE @xdl NVARCHAR(MAX) = CASE WHEN @SrceXml IS NOT NULL THEN CONVERT(VARCHAR(MAX), @SrceXml) ELSE @SrceDesc END                                                   
-                                          
+                                             
 IF OBJECT_ID('tempdb..#cox') IS NOT NULL                   
     DROP TABLE #cox        
 END                         
