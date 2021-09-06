162531938ce8b832090098d1c5f64064d4c473cc
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-09-06 11:46:46 +0300

    Update ActualPlan.Execute.sql

--- ActualPlan.Execute.sql
+++ ActualPlan.Execute.sql
@@ -30,7 +30,7 @@ DECLARE @plan XML = N'<?xml version="1.0" encoding="utf-16"?>
                 <ColumnReference Column="Expr1002" />                                                                                                                         
                  </OutputList>                            
               <RunTimeInformation>                       
-                <RunTimeCountersPerThread Thread="0" ActualRows="1" Batches="0" ActualEndOfScans="1" ActualExecutions="1" ActualExecutionMode="Row" ActualElapsedms="29123" ActualCPUms="0" />
+                <RunTimeCountersPerThread Thread="0" ActualRows="1" Batches="0" ActualEndOfScans="1" ActualExecutions="1" ActualExecutionMode="Row"   ="29123" ActualCPUms="0" />
               </RunTimeInformation>               
               <Parallelism>
                 <RelOp AvgRowSize="11" EstimateCPU="0" EstimateIO="0" EstimateRebinds="0" EstimateRewinds="0" EstimatedExecutionMode="Batch" EstimateRows="1" LogicalOp="Compute Scalar" NodeId="1" Parallel="true" PhysicalOp="Compute Scalar" EstimatedTotalSubtreeCost="31.6956">   
