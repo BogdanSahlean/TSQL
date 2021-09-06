227715369b262b149589a1d33bd6d624869b43a6
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-09-06 16:50:02 +0300

    Update sp_who4.sql

--- sp_who4.sql
+++ sp_who4.sql
@@ -28,7 +28,7 @@ BEGIN
 		UNION ALL                                                 
 		SELECT	blk_blk.session_id, NULL AS blocked_by, ROW_NUMBER() OVER(ORDER BY blk_blk.session_id)  AS group_num                                                                                                                             
 		FROM (                                                                         
-			SELECT	blk_sei.spid AS    session_id                        
+			SELECT	blk_sei.spid AS    session_id                          
 			FROM	sys.sysprocesses blk_sei                                                                                                                                                                                           
 			WHERE	EXISTS(SELECT * FROM s   ys.dm_os_waiting_tasks dmowt WHERE dmowt.blocking_session_id = blk_sei.spid) -- blk_sei.blocked = 0                                                                                                                                                                                                                                               
 			AND		NOT EXISTS(SELECT * FROM sys.dm_os_waiting_tasks dmowt WHERE dmowt.session_id = blk_sei.spid) -- blk_sei.blocked = 0                             
