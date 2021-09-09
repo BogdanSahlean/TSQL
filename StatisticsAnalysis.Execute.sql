44a6da0f9e02852823b729953ffd6bb49f4fedca
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-09-09 13:59:52 +0300

    Update StatisticsAnalysis.Execute.sql

--- StatisticsAnalysis.Execute.sql
+++ StatisticsAnalysis.Execute.sql
@@ -31,7 +31,7 @@ DECLARE @plan XML = N'<?xml version="1.0" encoding="utf-16"?>
             <WaitStats>                                    
               <Wait WaitType="PAGEIOLATCH_SH" WaitTimeMs="135917" WaitCount="1942" />                                                                                                                                                                                                
               <Wait WaitType="CXPACKET" WaitTimeMs="38607" WaitCount="9" />                                                                        
-                  <Wait WaitType="LATCH_SH" WaitTimeMs="19" WaitCount="5" />                                                                                                                                                          
+                  <Wait WaitType="LATCH_SH" WaitTimeMs="19" WaitCount="5" />                                                                                                                                                             
               <Wait WaitType="LATCH_EX" WaitTimeMs="13" WaitCount="40" />
               <Wait WaitType="SOS_SCHEDULER_YIELD" WaitTimeMs="8" WaitCount="1476" />
               <Wait WaitType="SESSION_WAIT_STATS_CHILDREN" WaitTimeMs="1" WaitCount="7" />
