d6c99f901b67de1f38162b090969911e95ad3280
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-09-18 09:41:40 +0300

    Update StatisticsAnalysis.Execute.sql

--- StatisticsAnalysis.Execute.sql
+++ StatisticsAnalysis.Execute.sql
@@ -42,7 +42,7 @@ DECLARE @plan XML = N'<?xml version="1.0" encoding="utf-16"?>
               <ComputeScalar>               
                 <DefinedValues>                                                                                                                                                                                                                                                                                              
                          
-                               
+                                  
               
                   <DefinedValue>         
                     <ColumnReference Column="Expr1002" />                                          
