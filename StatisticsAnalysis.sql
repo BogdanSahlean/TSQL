29f564537cd687ed44ea56b0557de16aea4693aa
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-09-21 01:58:34 +0300

    Update StatisticsAnalysis.sql

--- StatisticsAnalysis.sql
+++ StatisticsAnalysis.sql
@@ -8,7 +8,7 @@ AS
 BEGIN
 	DECLARE @TIME BIT = 0
 
-	SELECT @statistics = '<i>' + REPLACE(@statistics, CHAR(13)+CHAR(10), '</i><i>') + '</i>'   
+	SELECT @statistics = '<i>' + REPLACE(@statistics, CHAR(13)+CHAR(10), '</i><i>') + '</i>'      
 	PRINT @statistics
 
 	DECLARE @xms XML = CONVERT(XML, @statistics)    
