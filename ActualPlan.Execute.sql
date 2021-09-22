3166da41f9262e6976067dcc256577414b3cf399
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-09-22 20:52:02 +0300

    Create ActualPlan.Execute.sql

--- ActualPlan.Execute.sql
+++ ActualPlan.Execute.sql
@@ -1,8 +1,8 @@
       SET ANSI_NULLS ON            
 SET QUOTED_IDENTIFIER ON                     
-SET NUMERIC_ROUNDABORT OFF                   
+SET NUMERIC_ROUNDABORT OFF              
                                                     
-                                          
+                                           
 DECLARE @plan XML = N'<?xml version="1.0" encoding="utf-16"?>            
 <ShowPlanXML xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XML   Schema" Version="1.539" Build="15.0.2000.5"  
    xmlns="http://schemas.microsoft.com/sqlserver/2004/07/showplan">      
@@ -25,7 +25,7 @@ DECLARE @plan XML = N'<?xml version="1.0" encoding="utf-16"?>
                <RelOp AvgRowSize="11" EstimateCPU="0.0285019" EstimateIO="0" EstimateRebinds="0" EstimateRewinds="0" EstimatedExecutionMode="Row" Estim      ateRo ws="1" LogicalOp="Gather Streams" NodeId="0" Parallel="true" PhysicalOp="Parallelism" EstimatedTotalSubtreeCost="31.4377">                                                                                                                                                                                 
               <OutputList>                                             
                 <ColumnReference Column="Expr1002" />              
-                 </OutputList>                                                                                                                                                                                            
+                 </OutputList>                                                                                                                                                                                          
               <RunTimeInformation>                                                                                                                                                                                                                               
                 <RunTimeCountersPerThread Thread="0" ActualRows="1" Batches="0" ActualEndOfScans="1" ActualExecutions="1" ActualExecutionMode="Row"   ="29123" Actua  lCPUms="0" />
               </RunTimeInformation>                                
