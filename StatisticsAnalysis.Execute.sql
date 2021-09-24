7202ee08d01a6c0e649a26fc29ee721cede629f9
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-09-25 00:49:44 +0300

    Update StatisticsAnalysis.Execute.sql

--- StatisticsAnalysis.Execute.sql
+++ StatisticsAnalysis.Execute.sql
@@ -9,7 +9,7 @@ Table ''Worktable''. Scan count 0, logical reads 0, physical reads 0, page serve
        
    SQL Server Execution Times:                                             CPU time = 10557   mcs,  elapsed time = 1733 ms.                                                 
                                                                        
-(1 row affected)                                                                        
+(1 row affected)                                                                           
 Table ''FactOnlineSales''. Scan count 9, logical reads 28523, physical reads 0,       page server reads 0, re   ad-ahead  rea   ds 0, page s           erv   er read-ahead     0, lob logical reads 0, lob physical reads 0, lob page server reads 0, lob read-ahead reads 0, lob page server read-ahead reads 0.
                           
  SQL Server Execution Times:                                       
