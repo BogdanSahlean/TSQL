d1a23362f0501f2b52d34a9152f1297336683854
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-09-25 11:59:32 +0300

    Update StatisticsAnalysis.Execute.sql

--- StatisticsAnalysis.Execute.sql
+++ StatisticsAnalysis.Execute.sql
@@ -1,6 +1,6 @@
 DECLARE @sttcs NVARCHAR(MAX) =            
 'SQL Server parse and compile time:                                                              
-       SQL Server Execution Times:                                               
+       SQL Server Execution Times:                                                  
    CPU time = 0 ms,  elapsed time = 0 ms.                                                                                          
    (1 row affected)       
 Table ''FactOnlineSales''. Scan count 2555, logical reads 36240, physical reads 0, page server reads 0, read-ahead reads 0, page server read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob page server reads 0, lob read-ahead reads 0, lob page server read-ahead reads 0.
@@ -28,7 +28,7 @@ DECLARE @plan XML = N'<?xml version="1.0" encoding="utf-16"?>
             <OptimizerHardwareDependentProperties EstimatedAvailableMemoryGrant="155812" EstimatedPagesCached="77906" EstimatedAvailableDegreeOfParallelism="4" MaxCompileMemory="3248368" />
             <WaitStats>                                                         
               <Wait WaitType="PAGEIOLATCH_SH" WaitTimeMs="135917" WaitCount="1942" />                                                                                                                                                                                                                                                                                                                        
-                    <Wait WaitType="LATCH_SH" WaitTimeMs="19" WaitCount="5" />                                                                                                                                                                                                                                                                                                                                     
+                    <Wait WaitType="LATCH_SH" WaitTimeMs="19" WaitCount="5" />                                                                                                                                                                                                                                                                                                                                        
               <Wait WaitType="LATCH_EX" WaitTimeMs="13" WaitCount="40" />                                                   
               <Wait WaitType="SOS_SCHEDULER_YIELD" WaitTimeMs="8" WaitCount="1476" />                            
               <Wait WaitType="SESSION_WAIT_STATS_CHILDREN" WaitTimeMs="1" WaitCount="7" />
