61fb1fb9a50b3c81ac6c5c7de4bb9d3ea21179d1
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-09-25 11:51:15 +0300

    Update ActualPlan.Execute.sql

--- ActualPlan.Execute.sql
+++ ActualPlan.Execute.sql
@@ -1,7 +1,7 @@
       SET ANSI_NULLS ON               
 SET QUOTED_IDENTIFIER ON                        
 SET NUMERIC_ROUNDABORT OFF                      
-                                                          
+                                                               
                                                       
 DECLARE @plan XML = N'<?xml version="1.0" encoding="utf-16"?>              
 <ShowPlanXML xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XML   Schema" Version="1.539" Build="15.0.2000.5"  
@@ -19,7 +19,7 @@ DECLARE @plan XML = N'<?xml version="1.0" encoding="utf-16"?>
             <OptimizerHardwareDependentProperties EstimatedAvailableMemoryGrant="155812" EstimatedPagesCached="77906" EstimatedAvailableDegreeOfParallelism="4" MaxCompil   eMemory="4331840" />
             <WaitStats>      
               <Wait WaitType="PAGEIOLATCH_SH" WaitTimeMs="219756" WaitCount="1822" />             
-              <Wait WaitType="MEMORY_ALLOCATION_EXT" WaitTimeMs="4030" WaitCount="208734" />                                                                                                                               
+              <Wait WaitType="MEMORY_ALLOCATION_EXT" WaitTimeMs="4030" WaitCount="208734" />                                                                                                                                  
               <Wait WaitType="RESERVED_MEMORY_ALLOCATION_EXT" WaitTimeMs="2" WaitCount="182" />                                                                                                                                                                                                                                                  
                <QueryTimeStats CpuTime="11398" ElapsedTime="29138" /   >                                                               
                <RelOp AvgRowSize="11" EstimateCPU="0.0285019" EstimateIO="0" EstimateRebinds="0" EstimateRewinds="0" EstimatedExecutionMode="Row" Estim      ateRo ws="1" LogicalOp="Gather Streams" NodeId="0" Parallel="true" PhysicalOp="Parallelism" EstimatedTotalSubtreeCost="31.4377">                                                                                                                                                                                 
