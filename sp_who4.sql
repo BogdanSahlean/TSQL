4198646e82f328ab71f6b299565771cabcb7e5d2
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-10-08 12:35:49 +0300

    Update sp_who4.sql

--- sp_who4.sql
+++ sp_who4.sql
@@ -4,7 +4,7 @@ GO
 IF OBJECT_ID('sp_who4') IS NOT NULL
 BEGIN
 	DROP PROC sp_who4 
-END
+END   
 GO
 CREATE PROC sp_who4
 @extractindexes INT = NULL --NULL=No, 1=Execution Plans, 2=XML Missing Indexes, 3=Indexes SQL Statements
