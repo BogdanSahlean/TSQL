dee4385eebe29fa27b4c2ddc2e1fb559291cd2de
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-12-16 17:50:05 +0200

    Update ActualPlan.Execute.sql

--- ActualPlan.Execute.sql
+++ ActualPlan.Execute.sql
@@ -20,7 +20,7 @@ DECLARE @plan XML = N'<?xml version="1.0" encoding="utf-16"?>
             <WaitStats>      
               <Wait WaitType="PAGEIOLATCH_SH" WaitTimeMs="219756" WaitCount="1822" />                                   
               <Wait WaitType="MEMORY_ALLOCATION_EXT" WaitTimeMs="4030" WaitCount="208734" />                                                                                                                                      
-              <Wait WaitType="RESERVED_MEMORY_ALLOCATION_EXT" WaitTimeMs="2" WaitCount="182" />                                                                                                                                                                                                                                                  
+              <Wait WaitType="RESERVED_MEMORY_ALLOCATION_EXT" WaitTimeMs="2" WaitCount="182" />                                                                                                                                                                                                                                                     
                <QueryTimeStats CpuTime="11398" ElapsedTime="29138" /   >                                                               
                <RelOp AvgRowSize="11" EstimateCPU="0.0285019" EstimateIO="0" EstimateRebinds="0" EstimateRewinds="0" EstimatedExecutionMode="Row" Estim      ateRo ws="1" LogicalOp="Gather Streams" NodeId="0" Parallel="true" PhysicalOp="Parallelism" EstimatedTotalSubtreeCost="31.4377">                                                                                                                                                                                 
               <OutputList>            
