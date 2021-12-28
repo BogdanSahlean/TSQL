1c417ce5f3c35760f1ffbe28ae4c3eb847d8326e
BogdanSahlean <62909052+BogdanSahlean@users.noreply.github.com>
2021-12-28 15:47:38 +0200

    Update sp_who4.sql

--- sp_who4.sql
+++ sp_who4.sql
@@ -18,19 +18,19 @@ BEGIN
 	BEGIN 
 		DROP TABLE #resc;
 	END;
-                  
+                     
 	CREATE TABLE #sessions (
 		group_num				INT,
 		hid						HIERARCHYID,
 		blocking_session	VARCHAR(4000),
 		session_id				INT
-	);
+	);   
 
 	WITH BlkSessions
 	AS (
 		SELECT	blk_sei.spid AS session_id, NULLIF(blk_sei.blocked, 0) AS blocked_by, NULL AS group_num                
 		FROM	sys.sysprocesses blk_sei            
-		WHERE	blk_sei.blocked <> 0                                                                                             
+		WHERE	blk_sei.blocked <> 0                                                                                                
 		                     
 		UNION ALL 
 		SELECT	blk_blk.session_id, NULL AS blocked_by, ROW_NUMBER() OVER(ORDER BY blk_blk.session_id)  AS group_num
